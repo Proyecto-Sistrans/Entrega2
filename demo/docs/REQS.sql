98556e256d0f705f0c0895476885a12d422cdada
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2025-04-27 22:51:42 -0500

    consultas docs

--- demo/docs/REQS.sql
+++ demo/docs/REQS.sql
@@ -49,4 +49,37 @@ AND a.fecha BETWEEN :fecha_inicio AND :fecha_fin
 ORDER BY a.fecha;
 
 
+-- RFC5 y RFC6- CONSULTAR LA AGENDA DE DISPONIBILIDAD DE UN SERVICIO DE SALUD
+SELECT s.nombre AS servicio, 
+       a.fecha AS disponibilidad, 
+       m.nombre AS medico, 
+       i.nombre AS ips 
+FROM Agendas a 
+JOIN Servicios s ON a.servicio_cita = s.id 
+JOIN Medicos m ON a.medico_cita = m.registro_medico 
+JOIN Contratados_En ce ON m.registro_medico = ce.registro_medico 
+JOIN IPS i ON ce.nit = i.nit 
+WHERE a.estado = 'disponible' 
+AND (:codigoServicio IS NULL OR a.servicio_cita = :codigoServicio) 
+AND (:codigoMedico IS NULL OR a.medico_cita = :codigoMedico) 
+AND a.fecha BETWEEN TO_DATE(:fechaInicio, 'YYYY-MM-DD') AND TO_DATE(:fechaFin, 'YYYY-MM-DD') 
+ORDER BY a.fecha;
+
+
+--RF7 -- Tiene en cuenta las 4 semanas siguientes a la fecha actual
+SELECT s.nombre AS servicio, 
+       a.fecha AS disponibilidad, 
+       m.nombre AS medico, 
+       i.nombre AS ips 
+FROM Agendas a 
+JOIN Servicios s ON a.servicio_cita = s.id 
+JOIN Medicos m ON a.medico_cita = m.registro_medico 
+JOIN Contratados_En ce ON m.registro_medico = ce.registro_medico 
+JOIN IPS i ON ce.nit = i.nit 
+WHERE a.estado = 'disponible' 
+AND (:codigoServicio IS NULL OR a.servicio_cita = :codigoServicio) 
+AND (:codigoMedico IS NULL OR a.medico_cita = :codigoMedico) 
+AND a.fecha BETWEEN TO_DATE(:fechaInicio, 'YYYY-MM-DD') AND ADD_MONTHS(TO_DATE(:fechaInicio, 'YYYY-MM-DD'), 1) 
+ORDER BY a.fecha;
+
 
